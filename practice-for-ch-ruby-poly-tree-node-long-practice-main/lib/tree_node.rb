c196d36d9b7db450bd34be7a92fcc1db3644a1f6
Andrew Lipuma <andrewlipuma@gmail.com>
2023-08-04 12:26:53 -0700

    Implemented phase 1 of PolyTreeNode

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,20 @@
+require "set"
+
 class PolyTreeNode
-  
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    attr_reader :parent, :children, :value
+
+    attr_writer :children
+
+    def parent=(node_instance)
+        if node_instance 
+            @parent = node_instance
+            node_instance.children << self if node_instance.children.each {|child| child.value != self.value}
+        end
+    end
 end
